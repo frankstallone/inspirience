ab96ee0f80ff046b7a6b236099e7cd13f21a0c38
Frank Stallone III <frank@stallone.dev>
2021-06-07 21:43:58 -0400

    config: 🔧 Apple notch maybe

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,13 @@ const IndexPage = () => {
         bodyAttributes={{
           class: "overflow-x-hidden",
         }}
-      ></Helmet>
+      >
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="viewport" content="initial-scale=1 viewport-fit=cover" />
+      </Helmet>
       <header className="min-h-screen bg-gradient-to-br from-yellow-300 to-pink-600 flex flex-col flex-1 justify-center items-center">
         <h1 className="text-9xl tracking-wider font-thin mix-blend-overlay">
           Inspirience
