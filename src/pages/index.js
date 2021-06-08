b7d84b3cef53aa4a347c4abbf464d2068d59fa41
Frank Stallone III <frank@stallone.dev>
2021-06-08 07:16:17 -0400

    refactor: ♻️  Responsive font size

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const IndexPage = () => {
         />
       </Helmet>
       <header className="min-h-screen bg-gradient-to-br from-yellow-300 to-pink-600 flex flex-col flex-1 justify-center items-center">
-        <h1 className="text-9xl tracking-wider font-thin mix-blend-overlay">
+        <h1 className="text-4xl md:text-6xl lg:text-9xl tracking-wider font-thin mix-blend-overlay">
           Inspirience
         </h1>
       </header>
